2a0cbcd2c4b07be6045533d5ca7b7b0072cc4c2f
chuvu13012003 <103091131+chuvu13012003@users.noreply.github.com>
2022-04-25 09:35:40 +0700

    Create syring-with-quotes.js

---
+++ syring-with-quotes.js
@@ -0,0 +1,8 @@
+var title;
+var message;
+title = "Molly's Speccial Offers";
+message = '<a href="sale.html">25% off!</a>';
+var elTitle = document.getElmentById('title');
+elTitle.textContent = titlr;
+var elNote = document.getElmentById('note');
+elNote.innerHTML = message;
